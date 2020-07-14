b0266ccd08ec71ea24b2fa44cee9f4477ff72183
Kara Gruenewald <karakilkenny@karas-imac.attlocal.net>
2020-07-14 15:19:39 -0500

    ch13 razor walkthrough

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+
+        static List<string> Events = new List<string>();
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+       
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/events/add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+            return Redirect("/events");
+        }
+    }
+}
