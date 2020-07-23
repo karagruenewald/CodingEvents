72e986c2a5aa1b27b6da69ac7a73a6fa04108125
Kara Gruenewald <karakilkenny@karas-imac.attlocal.net>
2020-07-23 11:20:10 -0500

    completed ch19 studio

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,18 +13,46 @@ namespace CodingEvents.Controllers
 {
     public class EventCategoryController : Controller
     {
-        private EventDbContext eventCategory;
+        private EventDbContext context;
 
         public EventCategoryController(EventDbContext dbContext)
         {
-            eventCategory = dbContext;
+            context = dbContext;
         }
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<EventCategory> categories = eventCategory.EventCategories.ToList();
+            List<EventCategory> categories = context.EventCategories.ToList();
             return View(categories);
         }
+
+
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel category = new AddEventCategoryViewModel();
+            return View(category);
+        }
+
+        [HttpPost]
+        [Route("/eventcategory/created")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+
+                };
+
+                context.EventCategories.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("/eventcategory");
+            }
+
+            return View(addEventCategoryViewModel);
+        }
+
     }
 }
